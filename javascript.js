34ae2f4307b36026addacc9fec6130808e06e7b8
Henry Moore <hmoore@basecampcodingacademy.org>
2018-10-19 09:33:13 -0500

    Removed commented out code!

--- javascript.js
+++ javascript.js
@@ -1,35 +1,3 @@
-// function car1(STOCK_DATA) {
-//     var button = document.getElementById("chall");
-//     button.innerHTML = STOCK_DATA.repository.carChall;
-// }
-// function car2(STOCK_DATA) {
-//     var button = document.getElementById("must");
-//     button.innerHTML = STOCK_DATA.repository.carMust;
-// }
-// function car3(STOCK_DATA) {
-//     var button = document.getElementById("char");
-//     button.innerHTML = STOCK_DATA.repository.carChar;
-// }
-// function car4(STOCK_DATA) {
-//     var button = document.getElementById("cama");
-//     button.innerHTML = STOCK_DATA.repository.carCama;
-// }
-// function car5(STOCK_DATA) {
-//     var button = document.getElementById("corv");
-//     button.innerHTML = STOCK_DATA.repository.carCorv;
-// }
-// function car6(STOCK_DATA) {
-//     var button = document.getElementById("lamb");
-//     button.innerHTML = STOCK_DATA.repository.carLamb;
-// }
-// function car7(STOCK_DATA) {
-//     var button = document.getElementById("buga");
-//     button.innerHTML = STOCK_DATA.repository.carBuga;
-// }
-// function car8(STOCK_DATA) {
-//     var button = document.getElementById("ferr");
-//     button.innerHTML = STOCK_DATA.repository.carFerr;
-// }
 function car(STOCK_DATA) {
     var div = document.querySelector("#template1").innerHTML;
     var template = Handlebars.compile(div);
@@ -45,12 +13,6 @@ function car(STOCK_DATA) {
     }
 }
 
-// function car(STOCK_DATA, carType, buttonId) {
-//     var cars = document.querySelectorAll(".car");
-//     for (const car of cars) {
-//         car.addEventListener("click", )
-//     }
-//     }
 function pop() {
     var c = 0;
     var btn = document.querySelectorAll(".btn");
