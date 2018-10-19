fc8d1dd796444803ff09b85844cda23e956907c3
Henry Moore <hmoore@basecampcodingacademy.org>
2018-10-19 09:28:07 -0500

    DRY up car functions
    I summarized all the functions (requested in #1).

--- data.js
+++ data.js
@@ -1,12 +1,42 @@
-const STOCK_DATA = {
-    repository: {
-        carChall: "Challenger",
-        carMust: "Mustang",
-        carChar: "Charger",
-        carCama: "Camaro",
-        carCorv: "Corvette Z06",
-        carLamb: "Lamborghini",
-        carBuga: "Bugatti",
-        carFerr: "Ferrari"
+const STOCK_DATA = [
+    {
+        name: "Challenger",
+        imageURL: "images.jpeg",
+        price: "$200"
+    },
+    {
+        name: "Mustang",
+        imageURL: "images1.jpeg",
+        price: "$175"
+    },
+    {
+        name: "Charger",
+        imageURL: "images2.jpeg",
+        price: "$210"
+    },
+    {
+        name: "Camaro",
+        imageURL: "images3.jpeg",
+        price: "$225"
+    },
+    {
+        name: "Corvette Z06",
+        imageURL: "index4.jpeg",
+        price: "$250"
+    },
+    {
+        name: "Lamborghini",
+        imageURL: "images5.jpeg",
+        price: "$350"
+    },
+    {
+        name: "Bugatti",
+        imageURL: "images6.jpeg",
+        price: "$500"
+    },
+    {
+        name: "Ferrari",
+        imageURL: "images7.jpeg",
+        price: "$450"
     }
-};
+];
